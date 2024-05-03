ea9aa9b4e40f59d0dcddc6d289595dabea1ee8cf
Artem <a.g.sulimko@gmail.com>
2024-05-03 23:08:40 +0200

    s

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+// const DB_HOST =
+//   "mongodb+srv://Artem:aJfvNn5qUFyBGLPI@cluster0.z4pjdre.mongodb.net/db-contactbook";
+const { DB_HOST, PORT = 3000 } = process.env;
+mongoose.set("strictQuery", true);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful", "PORT:", PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1); // закрывает запущенные программы
+  });
