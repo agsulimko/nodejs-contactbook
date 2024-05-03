3251afed631f3c34cd379ff5581410060e3cd3c7
Artem <a.g.sulimko@gmail.com>
2024-05-03 23:21:42 +0200

    deploy bd render

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 const app = require("./app");
 // const DB_HOST =
 //   "mongodb+srv://Artem:aJfvNn5qUFyBGLPI@cluster0.z4pjdre.mongodb.net/db-contactbook";
-const { PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(
-    "mongodb+srv://Artem:aJfvNn5qUFyBGLPI@cluster0.z4pjdre.mongodb.net/db-contactbook"
-  )
+  .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
       console.log("Database connection successful", "PORT:", PORT);
