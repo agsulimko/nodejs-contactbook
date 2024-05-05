8ef6be8d576058acd6e6ec3946e0e30c63e64ba4
Artem <a.g.sulimko@gmail.com>
2024-05-05 19:48:42 +0200

    add controllers: ctrlWrapper, HttpError

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,57 @@
+// const express = require("express");
+
+// const router = express.Router();
+
+// router.get("/", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.get("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.post("/", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.delete("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.put("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// module.exports = router;
+
 const express = require("express");
+const ctrl = require("../../controllers/contacts");
+
+const { validateBody, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contacts");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/", ctrl.getAll);
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.post("/", validateBody(schemas.joiSchema), ctrl.add);
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.put(
+  "/:contactId",
+  isValidId,
+  validateBody(schemas.joiSchema),
+  ctrl.updateById
+);
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.patch(
+  "/:contactId/favorite",
+
+  validateBody(schemas.favoriteJoiSchema),
+  ctrl.updateStatusContact
+);
+
+router.delete("/:contactId", isValidId, ctrl.deleteById);
 
 module.exports = router;
