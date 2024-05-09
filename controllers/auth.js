42aeb18af6129d7ab202d35d53c5c54d6d2649c1
Artem <a.g.sulimko@gmail.com>
2024-05-09 22:46:04 +0200

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -41,6 +41,7 @@ const login = async (req, res) => {
   };
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  console.log(token);
   // ===
   // await User.findByIdAndUpdate(user._id);
   // ===
