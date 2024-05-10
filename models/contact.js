5fafa5c5d668098cc84365ba500e201709a92774
Artem <a.g.sulimko@gmail.com>
2024-05-10 11:36:34 +0200

    add SECRET_KEY=

--- models/contact.js
+++ models/contact.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
 const Joi = require("joi");
-const { handleMongooseError } = require("../middlewares");
+const { handleMongooseError } = require("../helpers");
 
 // const sexList = ["man", "woman"];
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,30 +1,36 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, authenticate, isValidId } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 
 const router = express.Router();
 
-router.get("/", ctrl.getAll);
+router.get("/", authenticate, ctrl.getAll);
 
-router.get("/:contactId", isValidId, ctrl.getById);
+router.get("/:contactId", authenticate, isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas.joiSchema), ctrl.add);
+router.post("/", authenticate, validateBody(schemas.joiSchema), ctrl.add);
 
 router.put(
   "/:contactId",
+
+  authenticate,
   isValidId,
+
   validateBody(schemas.joiSchema),
   ctrl.updateById
 );
 
 router.patch(
   "/:contactId/favorite",
+
+  authenticate,
+  isValidId,
   validateBody(schemas.favoriteJoiSchema),
   ctrl.updateStatusContact
 );
 
-router.delete("/:contactId", isValidId, ctrl.deleteById);
+router.delete("/:contactId", authenticate, isValidId, ctrl.deleteById);
 
 module.exports = router;
